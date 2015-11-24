88056ba4324b7aa8b64eee0004b29425f2f8928d
Romain Pouclet <romain.pouclet@gmail.com>
2015-11-24 16:50:05 -0500

    Create iOS and Mac framework targets

---
+++ SwifteriOS/SwifteriOS.h
@@ -0,0 +1,19 @@
+//
+//  SwifteriOS.h
+//  SwifteriOS
+//
+//  Created by Romain Pouclet on 2015-11-24.
+//  Copyright © 2015 Damian Kołakowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwifteriOS.
+FOUNDATION_EXPORT double SwifteriOSVersionNumber;
+
+//! Project version string for SwifteriOS.
+FOUNDATION_EXPORT const unsigned char SwifteriOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwifteriOS/PublicHeader.h>
+
+
