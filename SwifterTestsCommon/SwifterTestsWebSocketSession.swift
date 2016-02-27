f8bfc12c306a320e958d142074b844bf16cfde46
Damian Kołakowski <kolakowski.damian@gmail.com>
2016-02-27 17:23:48 +0100

    Fixed tests for WebSockets.

--- SwifterTestsCommon/SwifterTestsWebSocketSession.swift
+++ SwifterTestsCommon/SwifterTestsWebSocketSession.swift
@@ -30,17 +30,18 @@ class SwifterTestsWebSocketSession: XCTestCase {
     }
     
     func testParser() {
-        let session = HttpHandlers.WebSocketSession(TestSocket([]))
         
         do {
-            try session.readFrame(TestSocket([0]))
+            let session = HttpHandlers.WebSocketSession(TestSocket([0]))
+            try session.readFrame()
             XCTAssert(false, "Parser should throw an error if socket has not enough data for a frame.")
         } catch {
             XCTAssert(true, "Parser should throw an error if socket has not enough data for a frame.")
         }
         
         do {
-            try session.readFrame(TestSocket([0b0000_0001, 0b0000_0000, 0, 0, 0, 0]))
+            let session = HttpHandlers.WebSocketSession(TestSocket([0b0000_0001, 0b0000_0000, 0, 0, 0, 0]))
+            try session.readFrame()
             XCTAssert(false, "Parser should not accept unmasked frames.")
         } catch HttpHandlers.WebSocketSession.Error.UnMaskedFrame {
             XCTAssert(true, "Parse should throw UnMaskedFrame error for unmasked message.")
@@ -49,49 +50,56 @@ class SwifterTestsWebSocketSession: XCTestCase {
         }
         
         do {
-            let frame = try session.readFrame(TestSocket([0b1000_0001, 0b1000_0000, 0, 0, 0, 0]))
+            let session = HttpHandlers.WebSocketSession(TestSocket([0b1000_0001, 0b1000_0000, 0, 0, 0, 0]))
+            let frame = try session.readFrame()
             XCTAssert(frame.fin, "Parser should detect fin flag set.")
         } catch {
             XCTAssert(false, "Parser should not throw an error for a frame with fin flag set (\(error)")
         }
         
         do {
-            let frame = try session.readFrame(TestSocket([0b0000_0000, 0b1000_0000, 0, 0, 0, 0]))
+            let session = HttpHandlers.WebSocketSession(TestSocket([0b0000_0000, 0b1000_0000, 0, 0, 0, 0]))
+            let frame = try session.readFrame()
             XCTAssertEqual(frame.opcode, HttpHandlers.WebSocketSession.OpCode.Continue, "Parser should accept Continue opcode.")
         } catch {
             XCTAssertTrue(true, "Parser should accept Continue opcode without any errors.")
         }
         
         do {
-            let frame = try session.readFrame(TestSocket([0b0000_0001, 0b1000_0000, 0, 0, 0, 0]))
+            let session = HttpHandlers.WebSocketSession(TestSocket([0b0000_0001, 0b1000_0000, 0, 0, 0, 0]))
+            let frame = try session.readFrame()
             XCTAssertEqual(frame.opcode, HttpHandlers.WebSocketSession.OpCode.Text, "Parser should accept Text opcode.")
         } catch {
             XCTAssert(false, "Parser should accept Text opcode without any errors.")
         }
         
         do {
-            let frame = try session.readFrame(TestSocket([0b0000_0010, 0b1000_0000, 0, 0, 0, 0]))
+            let session = HttpHandlers.WebSocketSession(TestSocket([0b0000_0010, 0b1000_0000, 0, 0, 0, 0]))
+            let frame = try session.readFrame()
             XCTAssertEqual(frame.opcode, HttpHandlers.WebSocketSession.OpCode.Binary, "Parser should accept Binary opcode.")
         } catch {
             XCTAssert(false, "Parser should accept Binary opcode without any errors.")
         }
         
         do {
-            let frame = try session.readFrame(TestSocket([0b0000_1000, 0b1000_0000, 0, 0, 0, 0]))
+            let session = HttpHandlers.WebSocketSession(TestSocket([0b0000_1000, 0b1000_0000, 0, 0, 0, 0]))
+            let frame = try session.readFrame()
             XCTAssertEqual(frame.opcode, HttpHandlers.WebSocketSession.OpCode.Close, "Parser should accept Close opcode.")
         } catch {
             XCTAssert(false, "Parser should accept Close opcode without any errors.")
         }
         
         do {
-            let frame = try session.readFrame(TestSocket([0b0000_1001, 0b1000_0000, 0, 0, 0, 0]))
+            let session = HttpHandlers.WebSocketSession(TestSocket([0b0000_1001, 0b1000_0000, 0, 0, 0, 0]))
+            let frame = try session.readFrame()
             XCTAssertEqual(frame.opcode, HttpHandlers.WebSocketSession.OpCode.Ping, "Parser should accept Ping opcode.")
         } catch {
             XCTAssert(false, "Parser should accept Ping opcode without any errors.")
         }
         
         do {
-            let frame = try session.readFrame(TestSocket([0b0000_1010, 0b1000_0000, 0, 0, 0, 0]))
+            let session = HttpHandlers.WebSocketSession(TestSocket([0b0000_1010, 0b1000_0000, 0, 0, 0, 0]))
+            let frame = try session.readFrame()
             XCTAssertEqual(frame.opcode, HttpHandlers.WebSocketSession.OpCode.Pong, "Parser should accept Pong opcode.")
         } catch {
             XCTAssert(false, "Parser should accept Pong opcode without any errors.")
@@ -99,7 +107,8 @@ class SwifterTestsWebSocketSession: XCTestCase {
         
         for opcode in [3, 4, 5, 6, 7, 11, 12, 13, 14, 15] {
             do {
-                try session.readFrame(TestSocket([UInt8(opcode), 0b1000_0000, 0, 0, 0, 0]))
+            let session = HttpHandlers.WebSocketSession(TestSocket([UInt8(opcode), 0b1000_0000, 0, 0, 0, 0]))
+                try session.readFrame()
                 XCTAssert(false, "Parse should throw an error for unknown opcode: \(opcode)")
             } catch HttpHandlers.WebSocketSession.Error.UnknownOpCode(_) {
                 XCTAssert(true, "Parse should throw UnknownOpCode error for unknown opcode.")
