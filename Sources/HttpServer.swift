b8b09e01634143fe60af6609997bbd9ef2a2bae6
Damian Kołakowski <kolakowski.damian@gmail.com>
2016-10-04 21:37:11 +0200

    Version changed to 1.3.1 (plus pod spec).

--- Sources/HttpServer.swift
+++ Sources/HttpServer.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public class HttpServer: HttpServerIO {
     
-    public static let VERSION = "1.3.0"
+    public static let VERSION = "1.3.1"
     
     private let router = HttpRouter()
     
