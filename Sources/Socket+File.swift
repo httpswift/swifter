79f3deb435dc24c460de8cb9ebb465ddf1ede1bb
Damian Kołakowski <kolakowski.damian@gmail.com>
2017-03-19 21:00:10 +0100

    Cherry-pick from master/fdc2cff.

--- Sources/Socket+File.swift
+++ Sources/Socket+File.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-#if os(iOS) || os(Linux)    
+#if os(iOS) || os(tvOS) || os (Linux)
     struct sf_hdtr { }
     
     private func sendfileImpl(_ source: UnsafeMutablePointer<FILE>, _ target: Int32, _: off_t, _: UnsafeMutablePointer<off_t>, _: UnsafeMutablePointer<sf_hdtr>, _: Int32) -> Int32 {
@@ -35,7 +35,7 @@ extension Socket {
         var offset: off_t = 0
         var sf: sf_hdtr = sf_hdtr()
         
-        #if os(iOS) || os(Linux)
+        #if os(iOS) || os(tvOS) || os (Linux)
         let result = sendfileImpl(file.pointer, self.socketFileDescriptor, 0, &offset, &sf, 0)
         #else
         let result = sendfile(fileno(file.pointer), self.socketFileDescriptor, 0, &offset, &sf, 0)
