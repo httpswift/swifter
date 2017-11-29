4ba0b5cb697baf3e0dc5f09286ce6e2732f3dc40
Javier Cala Uribe <javier@arkelao.co>
2017-11-29 11:54:41 -0500

    Fix compilation error on Xcode 9.1

--- Sources/Socket+File.swift
+++ Sources/Socket+File.swift
@@ -19,7 +19,11 @@ import Foundation
             }
             var writeCounter = 0
             while writeCounter < readResult {
-                let writeResult = send(target, &buffer + writeCounter, readResult - writeCounter, Int32(MSG_NOSIGNAL))
+                #if os(Linux)
+                    let writeResult = send(target, &buffer + writeCounter, readResult - writeCounter, Int32(MSG_NOSIGNAL))
+                #else
+                    let writeResult = write(target, &buffer + writeCounter, readResult - writeCounter)
+                #endif
                 guard writeResult > 0 else {
                     return Int32(writeResult)
                 }
