b384deed49e9682c4c48f03d0795b68a013af8b5
Adam Kaplan <adamkaplan@oath.com>
2019-02-18 15:49:14 -0500

    Disambiguate swift method call

--- Sources/Errno.swift
+++ Sources/Errno.swift
@@ -10,6 +10,6 @@ import Foundation
 public class Errno {
     
     public class func description() -> String {
-        return String(cString: UnsafePointer(strerror(errno)))
+        return String(cString: UnsafePointer<UInt8>(strerror(errno)))
     }
 }
