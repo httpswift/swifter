f1400c08e4e8047b0dbfcdd14d30d991964b07e3
Hani Ewais <haewais@microsoft.com>
2024-03-13 17:48:32 -0400

    added backward support for swift tools 5.0

---
+++ Package@swift-5.0.swift
@@ -0,0 +1,38 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+  name: "Swifter",
+
+  products: [
+    .library(name: "Swifter", targets: ["Swifter"]),
+    .executable(name: "SwifterExample", targets: ["SwifterExample"])
+  ],
+
+  dependencies: [],
+
+  targets: [
+    .target(
+      name: "Swifter",
+      dependencies: [],
+      path: "Xcode/Sources"
+      ),
+
+    .target(
+      name: "SwifterExample",
+      dependencies: [
+        "Swifter"
+      ], 
+      path: "SwifterExample"
+    ),
+
+    .testTarget(
+      name: "SwifterTests", 
+      dependencies: [
+        "Swifter"
+      ], 
+      path: "Xcode/Tests"
+    )
+  ]
+)
